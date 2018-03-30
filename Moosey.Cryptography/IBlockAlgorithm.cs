627cc353b26b125876b5657d0671f6504bfe064a
Michael J. Gray <mgray@emitcode.com>
2018-03-30 14:45:09 -0700

    Work on the BCrypt binding. Started to add interfaces for the common API.

--- Moosey.Cryptography/IBlockAlgorithm.cs
+++ Moosey.Cryptography/IBlockAlgorithm.cs
@@ -22,28 +22,15 @@
  * SOFTWARE.
 */
 
-using System;
-using Moosey.Cryptography.BCrypt;
-
-namespace Moosey.Cryptography.Example
+namespace Moosey.Cryptography
 {
-    internal static class Program
+    public interface IBlockAlgorithm : ISymmetricAlgorithm
     {
-        private static void Main()
+        int BlockSize
         {
-            IntPtr algorithm;
-            uint result = BCryptCore.BCryptOpenAlgorithmProvider(out algorithm, "AES", null, 0);
-
-            IntPtr key;
-            byte[] secret = new byte[16];
-            result = BCryptCore.BCryptGenerateSymmetricKey(algorithm, out key, null, 0, secret, (ulong)secret.Length, 0);
-
-            byte[] plaintext = new byte[] { 0x6a, 0x84, 0x86, 0x7c, 0xd7, 0x7e, 0x12, 0xad, 0x07, 0xea, 0x1b, 0xe8, 0x95, 0xc5, 0x3f, 0xa3 };
-
-            byte[] ciphertext = new byte[plaintext.Length];
-
-            ulong pcbResult;
-            BCryptCore.BCryptEncrypt(key, plaintext, (ulong)plaintext.Length, IntPtr.Zero, null, 0, ciphertext, (ulong)ciphertext.Length, out pcbResult, 0);
+            get;
         }
+
+        void SetBlockSize(int blockSize);
     }
 }
--- Moosey.Cryptography/ISymmetricAlgorithm.cs
+++ Moosey.Cryptography/ISymmetricAlgorithm.cs
@@ -22,28 +22,12 @@
  * SOFTWARE.
 */
 
-using System;
-using Moosey.Cryptography.BCrypt;
-
-namespace Moosey.Cryptography.Example
+namespace Moosey.Cryptography
 {
-    internal static class Program
+    public interface ISymmetricAlgorithm
     {
-        private static void Main()
-        {
-            IntPtr algorithm;
-            uint result = BCryptCore.BCryptOpenAlgorithmProvider(out algorithm, "AES", null, 0);
+        IAlgorithmTransformer CreateEncryptor(byte[] key);
 
-            IntPtr key;
-            byte[] secret = new byte[16];
-            result = BCryptCore.BCryptGenerateSymmetricKey(algorithm, out key, null, 0, secret, (ulong)secret.Length, 0);
-
-            byte[] plaintext = new byte[] { 0x6a, 0x84, 0x86, 0x7c, 0xd7, 0x7e, 0x12, 0xad, 0x07, 0xea, 0x1b, 0xe8, 0x95, 0xc5, 0x3f, 0xa3 };
-
-            byte[] ciphertext = new byte[plaintext.Length];
-
-            ulong pcbResult;
-            BCryptCore.BCryptEncrypt(key, plaintext, (ulong)plaintext.Length, IntPtr.Zero, null, 0, ciphertext, (ulong)ciphertext.Length, out pcbResult, 0);
-        }
+        IAlgorithmTransformer CreateDecryptor(byte[] key);
     }
 }
