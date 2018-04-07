88ecd45bdb4937a2fd665d13627ad2c8ec5ecd03
Michael J. Gray <mgray@emitcode.com>
2018-04-06 20:17:13 -0700

    Fixed marshalling behavior for BCryptEncrypt and made the rest explicit.

--- Moosey.Cryptography/BCrypt/BCryptCore.cs
+++ Moosey.Cryptography/BCrypt/BCryptCore.cs
@@ -32,49 +32,49 @@ namespace Moosey.Cryptography.BCrypt
         // https://msdn.microsoft.com/en-us/library/windows/desktop/aa375479(v=vs.85).aspx
         [DllImport("bcrypt.dll")]
         public static extern uint BCryptOpenAlgorithmProvider(
-            out IntPtr phAlgorithm,
-            [MarshalAs(UnmanagedType.LPWStr)] string pszAlgId,
-            [MarshalAs(UnmanagedType.LPWStr)] string pszImplementation,
-            uint dwFlags);
+            [Out] out IntPtr phAlgorithm,
+            [In, MarshalAs(UnmanagedType.LPWStr)] string pszAlgId,
+            [In, MarshalAs(UnmanagedType.LPWStr)] string pszImplementation,
+            [In] uint dwFlags);
 
         // https://msdn.microsoft.com/en-us/library/windows/desktop/aa375377(v=vs.85).aspx
         [DllImport("bcrypt.dll")]
         public static extern uint BCryptCloseAlgorithmProvider(
-            ref IntPtr hAlgorithm,
-            ulong dwFlags);
+            [In, Out] ref IntPtr hAlgorithm,
+            [In] ulong dwFlags);
 
         // https://msdn.microsoft.com/en-us/library/windows/desktop/aa375504(v=vs.85).aspx
         [DllImport("bcrypt.dll")]
         public static extern uint BCryptSetProperty(
-            ref IntPtr hObject,
-            [MarshalAs(UnmanagedType.LPWStr)] string pszProperty,
-            byte[] pbInput,
-            ulong cbInput,
-            ulong dwFlags);
+            [In, Out] ref IntPtr hObject,
+            [In, MarshalAs(UnmanagedType.LPWStr)] string pszProperty,
+            [In] byte[] pbInput,
+            [In] ulong cbInput,
+            [In] ulong dwFlags);
 
         // https://msdn.microsoft.com/en-us/library/windows/desktop/aa375453(v=vs.85).aspx
         [DllImport("bcrypt.dll")]
         public static extern uint BCryptGenerateSymmetricKey(
-            IntPtr hAlgorithm,
-            out IntPtr phKey,
-            byte[] pbKeyObject,
-            ulong cbKeyObject,
-            byte[] pbSecret,
-            ulong cbSecret,
-            ulong dwFlags);
+            [In] IntPtr hAlgorithm,
+            [Out] out IntPtr phKey,
+            [Out] byte[] pbKeyObject,
+            [In] ulong cbKeyObject,
+            [In] byte[] pbSecret,
+            [In] ulong cbSecret,
+            [In] ulong dwFlags);
 
         // https://msdn.microsoft.com/en-us/library/windows/desktop/aa375421(v=vs.85).aspx
         [DllImport("bcrypt.dll")]
         public static extern uint BCryptEncrypt(
-            IntPtr hKey,
-            byte[] pbInput,
-            ulong cbInput,
-            IntPtr pPaddingInfo,
-            byte[] pvIv,
-            ulong cbIv,
-            byte[] pbOutput,
-            ulong cbOutput,
-            out ulong pcbReesult,
-            ulong dwFlags);
+            [In] IntPtr hKey,
+            [In] byte[] pbInput,
+            [In] ulong cbInput,
+            [In] IntPtr pPaddingInfo,
+            [In, Out] byte[] pvIv,
+            [In] ulong cbIv,
+            [Out] byte[] pbOutput,
+            [In] ulong cbOutput,
+            [Out] out ulong pcbReesult,
+            [In] ulong dwFlags);
     }
 }
